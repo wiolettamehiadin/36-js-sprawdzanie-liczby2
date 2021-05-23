fced160262fbeec9aedb249ae2e18ad9022b83a4
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-23 23:01:59 +0100

    Rozpoczecie pliku

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function compareNumbers (number1, number2) {
+if (number1 === 20) {
+    console.log('True');
+} else if (number2 === 20) {
+    console.log('True');
+} else if (number1 + number2 <= 20) {
+    console.log('Ture');
+} else {
+    console.log('False');
+}
+}
+
+compareNumbers (10, 20);
